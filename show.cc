3e91049720312642b968d1c55f03c2ef9735bad3
Stéphane Marchesin <stephane.marchesin@gmail.com>
2024-11-01 23:53:29 -0700

    Add support for "show" command

---
+++ show.cc
@@ -0,0 +1,70 @@
+#include "includes.h"
+
+#include "constants.h"
+#include "database.h"
+#include "diff.h"
+#include "pager.h"
+#include "show.h"
+#include "util.h"
+#include "view.h"
+
+static char* commit_id;
+
+static int show_update()
+{
+	if (!show_commit)
+		return 1;
+
+	move (0, 0);
+	attron (COLOR_PAIR (7));
+	for (int i = diff_cursor; i < min (diff_cursor + view_height (), (int) diff.size ()); i++) {
+		switch (diff[i][0]) {
+		case '-':
+			attron (A_BOLD);
+			attron (COLOR_PAIR (1));
+			break;
+		case '+':
+			attron (A_BOLD);
+			attron (COLOR_PAIR (2));
+			break;
+		case '@':
+			attron (A_BOLD);
+			attron (COLOR_PAIR (5));
+			break;
+		default:
+			attroff (A_BOLD);
+			attron (COLOR_PAIR (7));
+			break;
+		}
+		if (i == 0)
+			attron (A_BOLD);
+		printw ("%s\n", diff[i]);
+	}
+
+	return 0;
+}
+
+static int show_get_size()
+{
+	return 0;
+}
+
+static void show_get_commit (int cursor, char node[])
+{
+	strcpy(node, commit_id);
+}
+
+
+void show_run (int argc, char *argv[])
+{
+	if (argc == 0)
+		return;
+
+	commit_id = strdup(argv[0]);
+
+	show_commit = true;
+
+	pager_run (show_get_size, show_update, show_get_commit);
+}
+
+
---
+++ show.h
@@ -0,0 +1,6 @@
+#ifndef _SHOW_H_
+#define _SHOW_H_
+
+void show_run (int argc, char *argv[]);
+
+#endif
